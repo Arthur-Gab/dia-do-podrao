12fbb3edda0574e06f719fc5e5e8d5154d21890d
Arthur-Gab <arthurgabriel933@gmail.com>
2023-11-09 07:54:53 -0300

    Prevenindo que a página recarregue ao realizar a pesquisa no serach input

--- src/components/Filters/index.jsx
+++ src/components/Filters/index.jsx
@@ -1,13 +1,13 @@
-import QuickFilter from "../../layout/QuickFilter";
-import { LuSearch, LuX } from "react-icons/lu";
+import QuickFilter from '../../layout/QuickFilter';
+import { LuSearch, LuX } from 'react-icons/lu';
 
 function Filters(props) {
 	function getInputValue(e) {
 		props.onSearchItem(e.target.value);
 	}
 
-	function onResetInputValue() {
-		props.onSearchItem("");
+	function onResetInputValue(e) {
+		props.onSearchItem('');
 	}
 
 	return (
@@ -15,6 +15,9 @@ function Filters(props) {
 			<form
 				className="flex max-w-5xl w-full mx-auto items-center mb-6"
 				onReset={onResetInputValue}
+				onSubmit={(e) => {
+					e.preventDefault();
+				}}
 			>
 				<fieldset className="relative flex items-center pb-1 border-b-red border-b-2 w-full max-w-5xl mx-auto">
 					<LuSearch
@@ -51,26 +54,26 @@ function Filters(props) {
 			<div
 				className="flex gap-2 max-[528px]:overflow-x-scroll pb-4 h-fit min-[519px]:justify-center sm:gap-4 scrollbar-filter"
 				tabIndex={-1}
-				aria-label={"Pesquisa rápida"}
+				aria-label={'Pesquisa rápida'}
 			>
 				<QuickFilter
-					label={"hamburguers"}
-					btnState={props.availableFoodStuffs.includes("hamburguers")}
+					label={'hamburguers'}
+					btnState={props.availableFoodStuffs.includes('hamburguers')}
 					onClick={props.onFilterSection}
 				/>
 				<QuickFilter
-					label={"bebidas"}
-					btnState={props.availableFoodStuffs.includes("bebidas")}
+					label={'bebidas'}
+					btnState={props.availableFoodStuffs.includes('bebidas')}
 					onClick={props.onFilterSection}
 				/>
 				<QuickFilter
-					label={"combos"}
-					btnState={props.availableFoodStuffs.includes("combos")}
+					label={'combos'}
+					btnState={props.availableFoodStuffs.includes('combos')}
 					onClick={props.onFilterSection}
 				/>
 				<QuickFilter
-					label={"alcoolico"}
-					btnState={props.availableFoodStuffs.includes("alcoolico")}
+					label={'alcoolico'}
+					btnState={props.availableFoodStuffs.includes('alcoolico')}
 					onClick={props.onFilterSection}
 				/>
 			</div>
